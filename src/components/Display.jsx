0f5761cd31946627b47af17b7ed39b7d370d6ad9
vedansh pratap singh <vedanshpsingh@gmail.com>
2025-04-05 22:45:32 +0530

    minor update

--- src/components/Display.jsx
+++ src/components/Display.jsx
@@ -3,24 +3,24 @@ import { Masonry } from '@mui/lab';
 import "../stylesheets/Display.css"
 const Display = ({ images ,onEdit}) => {
     return (
-        <div className="container">
-        {images.length === 0 ? (
-          <p style={{ textAlign: "center" }}>No images in gallery</p>
-        ) : (
-          <Masonry columns={4} spacing={2}>
-            {images.map((img, index) => (
-              <img
-                key={index}
-                src={img}
-                alt={`Edited ${index}`}
-                onClick={() => onEdit(img)}
-                className="disp-img"
-              />
-            ))}
-          </Masonry>
-        )}
-      </div>
-    );
-  };
-  
-  export default Display;
+       <div className="container">
+      {images.length === 0 ? (
+        <p style={{ textAlign: "center", color: "white" }}>No images in gallery</p>
+      ) : (
+        <Masonry columns={4} spacing={2}>
+          {images.map((img, index) => (
+            <img
+              key={index}
+              src={img}
+              alt={`Edited ${index}`}
+              onClick={() => onEdit(img)}
+              className="disp-img"
+            />
+          ))}
+        </Masonry>
+      )}
+    </div>
+  );
+};
+
+export default Display;
